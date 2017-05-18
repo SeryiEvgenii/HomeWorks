785d5e9bd1ff328688e3c8cf4d6e4ff5ec5449ae
SeryiEvgenii <zhekajob@gmail.com>
2017-05-18 23:16:43 +0300

    HomeWork 2

---
+++ homeW2/src/main.js
@@ -0,0 +1,60 @@
+// 1.
+
+let numbers = [25, 1];
+let project = 'проект';
+let team = `команда`;
+let howMuch = 'Сколько';
+let sentence = 'нужно программистов чтобы сделать проект ?';
+
+let task1 = howMuch + ' ' + sentence + ' ' + numbers[1] + ', ' + numbers[0] + ', ' + team;
+
+console.log(task1)
+
+// 2.
+
+let array = [
+  'Он',
+  'был больше ни телом, к которому рано или поздно',
+  'он обнаружил',
+  'не',
+  'Так, когда Будда достиг Просветления',
+  '...',
+  'что больше',
+  'ощущает себя мишенью',
+  'не'
+];
+
+let homeSentence = array[4] + ' ' + array[2] + ' ' + array[6] + ' ' + array[3] + ' ' + array[7] + '. ' + array[0] + ' ' + array[8] + ' ' + array[1] + ' ' + array[5];
+
+console.log(homeSentence);
+
+// 3.
+
+let myObj = {};
+
+myObj.home = 12;
+myObj.entrance = 'two';
+myObj["floor"] = 4;
+myObj["apartment"] = 34;
+
+console.log(myObj);
+
+// 4.
+
+let sliceString = 'Ytube';
+
+console.log('You' + sliceString.slice(1));
+
+// 5.
+
+let frameworks = [4.7, 'Angular', '4Angular', 'React/Redux'];
+
+let x = 'google \released ' + 'new version\ '
+        +
+        frameworks[1]
+		+ 
+		Math.floor(frameworks[0])
+		+  
+        '\ But real speed is '
+		+ 
+		`${frameworks[frameworks.length - 1]}`;
