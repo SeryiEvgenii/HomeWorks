5492abdbfd66863bb880f048f0f613a770dea030
SeryiEvgenii <zhekajob@gmail.com>
2017-05-18 21:06:17 +0300

    Home work 1

---
+++ homeW1/src/main.js
@@ -0,0 +1,13 @@
+const phoneUserItem = {
+	firstName: 'Evgeniy',
+	lastName: 'Seryi',
+	phone: 0971503196,
+	moreInformation:{
+		secondPhone: '050 000 00 00',
+		workPhone: '067 777 55 44'
+	},
+	workNow: true,
+	devExperience: ['3 year', 'HTML', 'CSS', 'JS&JQuery Basic']
+};
+
+console.log(phoneUserItem);
